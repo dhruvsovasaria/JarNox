28cfcac0530e46a54ae72e144e8f3bda9f52636e
Dhruv Sovasaria <dhruvsovasaria34@gmail.com>
2025-05-06 01:40:09 +0530

    refactor: port change

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT ; 
+const PORT = process.env.PORT || 3000; 
 
 app.get('/', (req, res) => {
   res.send('Hello World');
